147228e7e6a433a69c520c061c255b2a6b42ed0a
Ashwin-Selvaraj <ashwin240899@gmail.com>
2025-06-08 20:30:44 +0530

    Added Automation to the release function of vesting schedule

--- constructor-args.js
+++ constructor-args.js
@@ -1,7 +1,8 @@
 // constructor-args.js
+const bscTestnetConfig = require('./deployments/BSCTestnet.json');
+
 module.exports = [
     ["0xf73687C4d37d363e8f97Fad2bd03bc7b68876A72", "0x8358847c2D38F97F2Fc20cfA83F61075C4BfD14A"], // _owners
     1, // _numConfirmationsRequired
-    "0xca7109E4d8685FAD420FdE08aA92472F5bEFa5FA" // _token (ERC20 token address)
-  ];
-  
+    bscTestnetConfig.CVTContractAddress // _token (ERC20 token address)
+];
